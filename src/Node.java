6e9a0935e60ff7f134852c9c2e2c8b6f41b5c3c1
yulandaz <yulandaaazheng@gmail.com>
2024-09-25 21:50:22 -0400

    The kitty database submission

--- src/Node.java
+++ src/Node.java
@@ -1,22 +1,55 @@
-// Implement the constructor
+/****************************************************************************
+ @file: Node.java
+ @description: This program implements the Node class
+ @author: Yulanda Zheng
+ @date: September 26 , 2024
+ ****************************************************************************/
 
+public class Node<E> {
+    private E element;
+    private Node<E> right;
+    private Node<E> left;
 
-// Implement the setElement method
+    // Implement the constructor
+    public Node(E element) {
+        this.element = element;
+        this.right = null;
+        this.left = null;
+    }
 
+    //Implement the getElement method
 
-// Implement the setLeft method
+    public E getElement() {
+        return element;
+    }
 
+    // Implement the setElement method
+    public void setElement(E element) {
+        this.element = element;
+    }
 
-// Implement the setRight method
+    // Implement the setLeft method
+    public void setLeft(Node<E> left) {
+        this.left = left;
+    }
 
+    // Implement the setRight method
+    public void setRight(Node<E> right) {
+        this.right = right;
+    }
 
-// Implement the getLeft method
+    // implement getRight method
+    public Node<E> getRight(){
+        return this.right;
+    }
 
+    //implement getLeft method
+    public Node<E> getLeft(){
+        return this.left;
+    }
 
-// Implement the getRight method
-
-
-// Implement the getElement method
-
-
-// Implement the isLeaf method
+    // Implement the isLeaf method
+    public boolean isLeaf(){
+        return this.left == null && this.right == null;
+    }
+}
--- src/Proj1.java
+++ src/Proj1.java
@@ -1,3 +1,10 @@
+/****************************************************************************
+ @file: Proj1.java
+ @description: This program implements the Proj1 class which runs the program
+ @author: Yulanda Zheng
+ @date: September 26 , 2024
+ ****************************************************************************/
+
 import java.io.FileNotFoundException;
 
 public class Proj1 {
